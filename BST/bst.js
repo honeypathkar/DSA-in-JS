8d89efb73895c17c3ba19375c97d0334610d6410
honeypatkar <honeypatkar70@gmail.com>
2024-05-22 13:58:01 +0530

    Insertion of node in bst

---
+++ BST/bst.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+}
+
+const bst = new BST();
+console.log(bst.isEmpty());
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+
+console.log(bst.isEmpty());
